863037c4f779b00be5f50fad6616fe69a51f3cdf
XPNguyenDL <xpnguyen240802@gmail.com>
2023-02-21 01:13:00 +0700

    Initial Model

---
+++ TatBlog.Data/Mappings/TagMap.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using TatBlog.Core.Entities;
+
+namespace TatBlog.Data.Mappings
+{
+    public class TagMap : IEntityTypeConfiguration<Tag>
+    {
+        public void Configure(EntityTypeBuilder<Tag> builder)
+        {
+            builder.ToTable("Tags");
+
+            builder.HasKey(x => x.Id);
+
+            builder.Property(t => t.Name)
+                .IsRequired()
+                .HasMaxLength(50);
+
+            builder.Property(t => t.Description)
+                .HasMaxLength(500);
+
+            builder.Property(t => t.UrlSlug)
+                .HasMaxLength(50)
+                .IsRequired();
+        }
+    }
+}
