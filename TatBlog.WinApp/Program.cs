7140c71953aa2a01c51e2141ae49b2560ef40e62
XPNguyenDL <xpnguyen240802@gmail.com>
2023-02-22 01:16:57 +0700

    UpdateDataSeeder

--- TatBlog.WinApp/Program.cs
+++ TatBlog.WinApp/Program.cs
@@ -3,18 +3,47 @@
 using TatBlog.Data.Contexts;
 using TatBlog.Data.Seeder;
 
-var conext = new BlogDbContext();
-var seeder = new DataSeeder(conext);
+var context = new BlogDbContext();
+var seeder = new DataSeeder(context);
 seeder.Initialize();
 
-var authors = conext.Authors.ToList();
+#region Show Authors
 
-Console.WriteLine("{0, -40}{1, -30}{2, -30}{3, 12:MM/dd/yyyy}",
-    "ID", "Full Name", "Email", "Joined Date");
+//var authors = conext.Authors.ToList();
 
-foreach (var author in authors)
+//Console.WriteLine("{0, -40}{1, -30}{2, -30}{3, 12:MM/dd/yyyy}",
+//    "ID", "Full Name", "Email", "Joined Date");
+
+//foreach (var author in authors)
+//{
+//    Console.WriteLine("{0, -40}{1, -30}{2, -30}{3, 12:MM/dd/yyyy}",
+//        author.Id, author.FullName, author.Email, author.JoinedDate);
+//}
+
+#endregion
+
+#region MyRegion
+
+var posts = context.Posts.Where(p => p.Published)
+    .OrderBy(p => p.Title)
+    .Select(p => new
+    {
+        Id = p.Id,
+        Title = p.Title,
+        ViewCount = p.ViewCount,
+        PostedDate = p.PostedDate,
+        Author = p.Author.FullName,
+        Category = p.Category.Name
+    }).ToList();
+
+foreach (var post in posts)
 {
-    Console.WriteLine("{0, -40}{1, -30}{2, -30}{3, 12:MM/dd/yyyy}",
-        author.Id, author.FullName, author.Email, author.JoinedDate);
+    Console.WriteLine("Id: {0}", post.Id);
+    Console.WriteLine("Title: {0}", post.Title);
+    Console.WriteLine("ViewCount: {0}", post.ViewCount);
+    Console.WriteLine("PostedDate: {0}", post.PostedDate);
+    Console.WriteLine("Author: {0}", post.Author);
+    Console.WriteLine("Category: {0}", post.Category);
+    Console.WriteLine("".PadRight(80, '-'));
 }
-
+#endregion
