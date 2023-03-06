f3153a9bd0e499b4b332d277806cd6e0f3530645
XPNguyenDL <xpnguyen240802@gmail.com>
2023-03-06 14:38:06 +0700

    CreateWebApp

---
+++ TatBlog.WebApp/Controllers/BlogController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace TatBlog.WebApp.Controllers
+{
+    public class BlogController : Controller
+    {
+        public IActionResult Index()
+        {
+            ViewBag.CurrentTime = DateTime.Now.ToString("HH:mm:ss");
+            return View();
+        }
+
+        public IActionResult About()
+        {
+            return View();
+        }
+    }
+}
