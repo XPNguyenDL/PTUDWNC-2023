5328fd98244eff1f5d7d9c3a9c60cc479038b3b2
XPNguyenDL <xpnguyen240802@gmail.com>
2023-03-07 13:18:45 +0700

    Finish Lab02-homework

--- TatBlog.WebApp/Program.cs
+++ TatBlog.WebApp/Program.cs
@@ -1,51 +1,16 @@
-﻿using Microsoft.EntityFrameworkCore;
-using TatBlog.Data.Contexts;
-using TatBlog.Data.Seeder;
-using TatBlog.Services.Blogs;
+﻿using TatBlog.WebApp.Extensions;
 
 var builder = WebApplication.CreateBuilder(args);
 {
-    builder.Services.AddControllersWithViews();
-
-    builder.Services.AddDbContext<BlogDbContext>(options =>
-        options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
-
-    builder.Services.AddScoped<IBlogRepository, BlogRepository>();
-    builder.Services.AddScoped<IDataSeeder, DataSeeder>();
-
+    builder.ConfigureMVC()
+        .ConfigureServicer();
 }
-
 
 var app = builder.Build();
 {
-    // middleware để hiển thị thông báo lỗi
-    if (app.Environment.IsDevelopment())
-    {
-        app.UseDeveloperExceptionPage();
-    }
-    else
-    {
-        app.UseExceptionHandler("/Blog/Error");
-
-        app.UseHsts();
-    }
-    // Để chuyển http sang https
-    app.UseHttpsRedirection();
-
-    // Phục vụ các yêu cầu liên quan tới tập tin tĩnh: images, css
-    app.UseStaticFiles();
-
-    app.UseRouting();
-
-    app.MapControllerRoute(
-        name: "default",
-        pattern: "{controller=Blog}/{action=Index}/{id?}");
-}
-
-using (var scope = app.Services.CreateScope())
-{
-    var seeder = scope.ServiceProvider.GetRequiredService<IDataSeeder>();
-    seeder.Initialize();
+    app.UseRequestPipeline();
+    app.UseBlogRoutes();
+    app.UseDataSeeder();
 }
 
 app.Run();
