7152d0276bef21cced75f4905450b2d0c3821193
XPNguyenDL <xpnguyen240802@gmail.com>
2023-03-09 02:33:38 +0700

    Add Method Send Email

--- TatBlog.WebApp/Controllers/NewsletterController.cs
+++ TatBlog.WebApp/Controllers/NewsletterController.cs
@@ -1,16 +1,20 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using TatBlog.Core.Entities;
 using TatBlog.Services.Blogs;
+using System.Net.Mail;
+using System.Net;
 
 namespace TatBlog.WebApp.Controllers
 {
     public class NewsletterController : Controller
     {
         private readonly ISubscriberRepository _subRepo;
+        private readonly IWebHostEnvironment _hostingEnvironment;
 
-        public NewsletterController(ISubscriberRepository subRepo)
+        public NewsletterController(ISubscriberRepository subRepo, IWebHostEnvironment hostingEnvironment)
         {
             _subRepo = subRepo;
+            _hostingEnvironment = hostingEnvironment;
         }
 
         [HttpGet]
@@ -23,12 +27,40 @@ namespace TatBlog.WebApp.Controllers
         [HttpPost]
         public async Task<IActionResult> Subscribe(string email)
         {
+            try
+            {
+                var subSuccess = await _subRepo.SubscribeAsync(email);
+                var sub = await _subRepo.GetSubscriberByEmail(email);
 
-            var subSuccess = await _subRepo.SubscribeAsync(email);
-            var sub = await _subRepo.GetSubscriberByEmail(email);
+                string wwwrootPath = _hostingEnvironment.WebRootPath;
+                string filePath = Path.Combine(wwwrootPath, "templates/emails/EmailSubscribe.html");
+                string fileContents = System.IO.File.ReadAllText(filePath);
+                
+                fileContents = fileContents.Replace("{link}", "https://localhost:7245/Newsletter/UnSubscribe");
 
-            ViewData["subSuccess"] = subSuccess;
-            return View(sub);
+                using (MailMessage mail = new MailMessage())
+                {
+                    mail.From = new MailAddress("noreply.email.dluconfession@gmail.com");
+                    mail.To.Add(sub.Email);
+                    mail.Subject = $"Xác nhận đăng ký nhận thông báo.";
+                    mail.Body = fileContents;
+                    mail.IsBodyHtml = true;
+
+                    using (SmtpClient smtp = new SmtpClient("smtp.gmail.com", 587))
+                    {
+                        smtp.Credentials = new NetworkCredential("noreply.email.dluconfession@gmail.com",
+                            "upngspyyxjhrpjqd");
+                        smtp.EnableSsl = true;
+                        smtp.Send(mail);
+                    }
+                }
+                ViewData["subSuccess"] = subSuccess;
+                return View(sub);
+            }
+            catch (Exception e)
+            {
+                return BadRequest();
+            }
         }
 
         [HttpGet]
