97b3897f82925d7705167963fedfc69ade68ff13
XPNguyenDL <xpnguyen240802@gmail.com>
2023-03-09 14:20:56 +0700

    Optimize code

--- TatBlog.Core/Entities/Author.cs
+++ TatBlog.Core/Entities/Author.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Runtime.InteropServices.JavaScript;
-using System.Text;
-using System.Threading.Tasks;
-using TatBlog.Core.Contracts;
+﻿using TatBlog.Core.Contracts;
 
 namespace TatBlog.Core.Entities
 {
