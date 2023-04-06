811619a2b05ec3df3d3fd6d255d6f2050935b872
Nguyễn Xuân Phát <2014478@dlu.edu.vn>
2023-04-07 00:24:48 +0700

    API blog repo

---
+++ fontend/src/Pages/Blog/PostByAuthor.jsx
@@ -0,0 +1,42 @@
+import React from "react";
+import { useLocation, useParams } from "react-router-dom";
+import { getPostByAuthor } from "../../Services/BlogRepository";
+import PostItem from "../../Components/PostItem";
+import { useState, useEffect } from "react";
+import Pager from "../../Components/Pager";
+
+export default function PostByAuthor() {
+  const params = useParams();
+  const [posts, setPosts] = useState([]);
+  const [metadata, setMetadata] = useState({});
+
+  function useQuery() {
+    const { search } = useLocation();
+    return React.useMemo(() => new URLSearchParams(search), [search]);
+  }
+
+  const query = useQuery(),
+    pageNumber = query.get("p") ?? 1,
+    pageSize = query.get("ps") ?? 10;
+
+
+  useEffect(() => {
+    fetchPostsByAuthorSlug();
+    async function fetchPostsByAuthorSlug() {
+      const data = await getPostByAuthor(params.slug, pageSize, pageNumber);
+      if (data) {
+        setPosts(data.items);
+        setMetadata(data.metadata);
+      } else setPosts([]);
+    }
+  }, [params]);
+
+  return (
+    <div className="p-4">
+      {posts.map((item) => {
+        return <PostItem key={item.id} postItem={item} />;
+      })}
+      <Pager postQuery="" metadata={metadata} />
+    </div>
+  );
+}
