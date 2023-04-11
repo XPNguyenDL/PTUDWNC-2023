2914962c8515931c8378ca81e2536ca048e109ca
Nguyễn Xuân Phát <2014478@dlu.edu.vn>
2023-04-12 00:31:49 +0700

    Add index export

---
+++ fontend/src/Components/Widget/index.jsx
@@ -0,0 +1,6 @@
+export { default as Archives } from './Archives';
+export { default as BestAuthor } from './BestAuthor';
+export { default as CategoriesWidget } from './CategoriesWidget';
+export { default as FeaturedPosts } from './FeaturedPosts';
+export { default as RandomPosts } from './RandomPosts';
+export { default as TagCloud } from './TagCloud';
