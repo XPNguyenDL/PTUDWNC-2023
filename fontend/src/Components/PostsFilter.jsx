90bfb2f027fce10c69f55e4f488d5e025bce1bbf
Nguyễn Xuân Phát <2014478@dlu.edu.vn>
2023-04-12 21:16:41 +0700

    Add filter post

--- fontend/src/Components/PostsFilter.jsx
+++ fontend/src/Components/PostsFilter.jsx
@@ -1,7 +1,58 @@
-import React from 'react'
+import React from "react";
+import { useEffect } from "react";
+import { useState } from "react";
+import { Form } from "react-bootstrap";
+import { getPostsByQueries } from "../Services/BlogRepository";
+import PostItem from "./PostItem";
+import Pager from "./Pager";
 
-export default function PostsFilter() {
+export default function PostsFilter({ postQuery }) {
+  const {
+    keyword,
+    tagSlug,
+    authorSlug,
+    categorySlug,
+    pageSize,
+    sortColumn,
+    sortOrder
+  } = postQuery;
+
+  const [posts, setPosts] = useState([]);
+  const [metadata, setMetadata] = useState({});
+  const [pageNumber, setPageNumber] = useState("");
+
+  const handleChangePage = (value) => {
+    setPageNumber((current) => current + value);
+  };
+
+  useEffect(async () => {
+    const queries = new URLSearchParams({
+      Published: true,
+      Unpublished: false,
+      PageNumber: pageNumber || 1,
+      PageSize: 1
+    });
+    categorySlug && queries.append("CategorySlug", categorySlug);
+    authorSlug && queries.append("AuthorSlug", authorSlug);
+    tagSlug && queries.append("TagSlug", tagSlug);
+    keyword && queries.append("Keyword", keyword);
+
+    const data = await getPostsByQueries(queries);
+    if (data) {
+      setPosts(data.items);
+      setMetadata(data.metadata);
+    } else {
+      setPosts([]);
+      setMetadata({});
+    }
+  }, [postQuery]);
+
   return (
-    <div>PostsFilter</div>
-  )
+    <div>
+      {posts.map((post) => (
+        <PostItem key={post.id} post={post} />
+      ))}
+      <Pager metadata={metadata} onPageChange={handleChangePage} />
+    </div>
+  );
 }
--- fontend/src/Pages/Admin/Post.jsx
+++ fontend/src/Pages/Admin/Post.jsx
@@ -1,7 +1,108 @@
-import React from 'react'
+import React from "react";
+import { useState } from "react";
+import { getPostsByQueries } from "../../Services/BlogRepository";
+import { useEffect } from "react";
+import { Loading, PagerAdmin, PostsFilterPane } from "../../Components";
+import { Table } from "react-bootstrap";
+import { Link } from "react-router-dom";
+import { AiOutlineEye, AiOutlineEyeInvisible } from "react-icons/ai";
 
 export default function Post() {
+  const [posts, setPosts] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
+  const [pageNumber, setPageNumber] = useState(1);
+  const [metadata, setMetadata] = useState({});
+  const [keyword, setKeyword] = useState("");
+  const [authorSlug, setAuthorSlug] = useState();
+  const [categorySlug, setCategorySlug] = useState();
+
+  const handleChangePage = (value) => {
+		setPageNumber((current) => current + value);
+		window.scroll(0, 0);
+	};
+
+  useEffect(() => {
+    fetchPosts();
+    async function fetchPosts() {
+      const queries = new URLSearchParams({
+        PageNumber: pageNumber || 1,
+        PageSize: 10,
+        ...(keyword && { Keyword: keyword }),
+        ...(authorSlug && { AuthorSlug: authorSlug }),
+        ...(categorySlug && { CategorySlug: categorySlug }),
+      });
+      const data = await getPostsByQueries(queries);
+      if (data) {
+        setPosts(data.items);
+        setMetadata(data.metadata);
+      } else {
+        setPosts([]);
+        setMetadata({});
+      }
+      setIsLoading(false);
+    }
+  }, [keyword, authorSlug, categorySlug, pageNumber]);
+
   return (
-    <div>Post</div>
-  )
+    <div>
+      <h1>Danh sách bài viết</h1>
+      <PostsFilterPane
+        setKeyword={setKeyword}
+        setAuthorSlug={setAuthorSlug}
+        setCategorySlug={setCategorySlug}
+      />
+      {isLoading ? (
+        <Loading />
+      ) : (
+        <>
+          <Table striped responsive bordered>
+            <thead>
+              <tr>
+                <th>Tiêu đề</th>
+                <th>Tác giả</th>
+                <th>Chủ đề</th>
+                <th>Xuất bản</th>
+              </tr>
+            </thead>
+            <tbody>
+              {posts.length > 0 ? (
+                posts.map((item) => {
+                  return (
+                    <tr key={item.id}>
+                      <td>
+                        <Link
+                          to={`admin/posts/edit/${item.id}`}
+                          className="text-bold">
+                          {item.title}
+                        </Link>
+                        <p className="text-muted">{item.shortDescription}</p>
+                      </td>
+                      <td>{item.author.fullName}</td>
+                      <td>{item.category.name}</td>
+                      <td>
+                        {item.published ? (
+                          <AiOutlineEye />
+                        ) : (
+                          <AiOutlineEyeInvisible />
+                        )}
+                      </td>
+                    </tr>
+                  );
+                })
+              ) : (
+                <tr>
+                  <td colSpan={4}>
+                    <h4 className="text-danger text-center">
+                      Không có bài viết nào
+                    </h4>
+                  </td>
+                </tr>
+              )}
+            </tbody>
+          </Table>
+          <PagerAdmin metadata={metadata} onPageChange={handleChangePage} />
+        </>
+      )}
+    </div>
+  );
 }
