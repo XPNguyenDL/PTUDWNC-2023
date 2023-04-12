6c3b8d53fd701ef62398137cc4caac088746e2b4
Nguyễn Xuân Phát <2014478@dlu.edu.vn>
2023-04-12 21:16:06 +0700

    Add loading component

--- fontend/src/Components/index.jsx
+++ fontend/src/Components/index.jsx
@@ -9,3 +9,4 @@ export { default as PostsFilter } from './PostsFilter';
 export { default as SearchForm } from './SearchForm';
 export { default as TagList } from './TagList';
 export { default as Utils } from './Utils';
+export { default as Loading } from './Loading';
