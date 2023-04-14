b68caa6c3ef38abc2fcbf79949ec1e8bb6ae1675
Nguyễn Xuân Phát <2014478@dlu.edu.vn>
2023-04-14 13:31:23 +0700

    Add page edit post

--- fontend/src/Pages/Admin/Index.jsx
+++ fontend/src/Pages/Admin/Index.jsx
@@ -3,4 +3,5 @@ export { default as Author } from './Author';
 export { default as Category } from './Category';
 export { default as Comment } from './Comment';
 export { default as Post } from './Post';
+export { default as PostEdit } from './PostEdit';
 export { default as Tags } from './Tags';
