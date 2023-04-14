85668a7a9cbec0697bf27e611a8a5961d755a945
Nguyễn Xuân Phát <2014478@dlu.edu.vn>
2023-04-14 15:27:08 +0700

    Fix bug

--- fontend/src/Components/Admin/index.jsx
+++ fontend/src/Components/Admin/index.jsx
@@ -2,3 +2,5 @@ export { default as Dashboard } from './Dashboard';
 export { default as NavbarAdmin } from './Navbar';
 export { default as PagerAdmin } from './PagerAdmin';
 export { default as PostsFilterPane } from './Posts/PostsFilterPane';
+export { default as CategoriesFilterPane } from './Categories/CategoriesFilterPane';
+export { default as AuthorFillterPane } from './Authors/AuthorFillterPane';
