70ade809bc2b614e450f950e46fef9cea2f2b9fe
Nguyễn Xuân Phát <2014478@dlu.edu.vn>
2023-04-14 13:30:05 +0700

    Add check Guid

--- fontend/src/Components/Utils.jsx
+++ fontend/src/Components/Utils.jsx
@@ -1,6 +1,13 @@
-export default function isEmptyOrSpaces(str) {
+export function isEmptyOrSpaces(str) {
   return str === null || (typeof str === "string" && str.match(/^ *$/) !== null);
 }
 
 export const API_URL = "https://localhost:7058"
 
+export function isGuid(str) {
+  // Regular expression for matching a GUID pattern
+  const guidPattern = /^[a-f\d]{8}(-[a-f\d]{4}){3}-[a-f\d]{12}$/i;
+  
+  // Test the input string against the pattern and return the result
+  return guidPattern.test(str);
+}
