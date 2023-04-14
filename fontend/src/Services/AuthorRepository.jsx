f840d1eed302d12a6cf9fd085b4c9c570abc7ebb
Nguyễn Xuân Phát <2014478@dlu.edu.vn>
2023-04-14 15:27:17 +0700

    Add author api

--- fontend/src/Services/AuthorRepository.jsx
+++ fontend/src/Services/AuthorRepository.jsx
@@ -9,3 +9,80 @@ export async function getAuthors() {
 	if (data.isSuccess) return data.result;
 	else return null;
 }
+
+export async function getAuthorsByQueries(parameters) {
+	try {
+	  const res = await axios.get(`${API_URL}/api/authors?${parameters}`);
+  
+	  const data = res.data;
+	  if (data.isSuccess) {
+		return data.result;
+	  } else {
+		return null;
+	  }
+	} catch (error) {
+	  return null;
+	}
+  }
+  
+  export async function deleteAuthor(id) {
+	try {
+	  const res = await axios.delete(`${API_URL}/api/authors/${id}`);
+  
+	  const data = res.data;
+	  if (data.isSuccess) {
+		return data.result;
+	  } else {
+		return null;
+	  }
+	} catch (error) {
+	  return null;
+	}
+  }
+  
+  export async function addAuthor(author) {
+	try {
+	  const res = await axios.post(`${API_URL}/api/authors`, author);
+	  const data = res.data;
+	  if (data.isSuccess) {
+		return data.result;
+	  } else {
+		console.log(data);
+		return null;
+	  }
+	} catch (error) {
+	  console.log("Error", error);
+	  return null;
+	}
+  }
+  
+  export async function updateAuthor(authorId, author) {
+	try {
+	  const res = await axios.put(`${API_URL}/api/authors/${authorId}`, author);
+	  const data = res.data;
+	  if (data.isSuccess) {
+		return data.result;
+	  } else {
+		return null;
+	  }
+	} catch (error) {
+	  console.log("Error", error);
+	  return null;
+	}
+  }
+  
+  export async function getAuthorById(id) {
+	  try {
+		  const reponse = await axios.get(`${API_URL}/api/authors/${id}`);
+		  const data = reponse.data;
+	  
+		  if (data.isSuccess) {
+			return data.result;
+		  } else {
+			return null;
+		  }
+		} catch (error) {
+		  console.log("Error", error.message);
+		  return null;
+		}
+  }
